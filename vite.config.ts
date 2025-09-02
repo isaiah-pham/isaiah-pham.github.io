93f26902eef69d92dfbcef5e7a6e5b29a5c558ac
CarelessWisp <isaiahpham04@gmail.com>
2025-09-02 14:24:45 -0400

    base change

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/isaiah-pham.github.io/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
